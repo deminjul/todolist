fdc5ac51d3e1e019f693ad5a6d6dcc0db499e707
deminjul <yulya.demina.01@gmail.com>
2025-04-27 21:04:55 +0300

    n
    n

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,18 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
-
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss],
+  plugins: [react() ,tailwindcss()],
+  base: "./",
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        entryFileNames: 'assets/[name]-[hash].js'
+      }
+    }
+  }
 })
-
