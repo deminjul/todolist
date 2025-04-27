d81cb409f3090f7289c78e1ff7ff2203f809cb48
deminjul <yulya.demina.01@gmail.com>
2025-04-27 22:05:48 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react() ,tailwindcss()],
-  base: "./",
+  plugins: [react()],
+  base: '/todolist/', // 1. Фиксим базовый путь
   build: {
     outDir: 'dist',
+    emptyOutDir: true, // 2. Очищаем папку перед сборкой
     assetsDir: 'assets',
     rollupOptions: {
       output: {
         assetFileNames: 'assets/[name]-[hash][extname]',
-        entryFileNames: 'assets/[name]-[hash].js'
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js' // 3. Добавляем для чанков
       }
     }
+  },
+  server: {
+    port: 5173
   }
 })
